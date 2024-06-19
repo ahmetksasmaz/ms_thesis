bb358620fe4b2244d2f6f125eaef0d7a12da9e81
Ahmet Kursad Sasmaz <ahmetksasmaz@gmail.com>
2024-06-19 14:28:29 +0300

    Experimentation structure is working well

--- experiment.py
+++ experiment.py
@@ -4,6 +4,10 @@ from gamma_applier import GammaApplier
 import cv2 as cv
 import tensorflow as tf
 import argparse
+import json
+import os
+import numpy as np
+import tqdm
 
 def psnr(y_true, y_pred):
     return tf.reduce_mean(tf.image.psnr(y_true, y_pred, max_val=255))
@@ -17,7 +21,8 @@ def main():
     parser.add_argument('-j','--dataset-json', help='Json database path for dataset', required=True, type=str)
     parser.add_argument('-c','--checkpoint-path', help='Checkpoint path of LACT /path/to/ckpt', required=True, type=str)
     parser.add_argument('-i','--input-resize-shape', help='Input resize shape w h', nargs="+", required=True, type=int)
-    parser.add_argument('-o','--output-path', help='Output directory of results', required=True, type=str)
+    parser.add_argument('-o','--output-file', help='Output file of results', required=True, type=str)
+    parser.add_argument('-p','--merged-predictions-path', help='Merged predictions directory path', required=True, type=str)
     args = vars(parser.parse_args())
     
     if len(args["input_resize_shape"]) != 2:
@@ -30,38 +35,110 @@ def main():
     clahe_applier = CLAHEApplier()
     gamma_applier = GammaApplier(100)
 
-    # Read dataset
+    dataset_json_file = open(args["dataset_json"])
+    dataset_json = json.load(dataset_json_file)
     
-    # Read results from json
-    # Transfer json to dict
+    results_dict = {}
     
-    # Iterate dataset
-        # Get Image and Label
-        # Resize Image and Label
-        # If image label tuple is in results then skip
-            # Lact Prediction
-            # PSNR & SSIM Lact
-            # Clahe
-            # PSNR & SSIM Clahe
-            # Gamma
-            # PSNR & SSIM Gamma
-            # Merge results and imwrite output
-            # Add results to dict
+    if os.path.isfile(args["output_file"]):
+        previous_output_file = open(args["output_file"])
+        for line in previous_output_file.readlines():
+            # Image path, algorithm [lact | clahe | gamma], psnr, ssim
+            fields = line.split("\t")
+            if fields[0] not in results_dict.keys():
+                results_dict[fields[0]] = {}
+            results_dict[fields[0]][fields[1]] = (float(fields[2]),float(fields[3]))
+        previous_output_file.close()
+    
+    output_file = open(args["output_file"], "a")
+    
+    for i in tqdm.tqdm(range(len(dataset_json["image"]))):
+        image_path = dataset_json["image"][i]
+        label_path = dataset_json["label"][i]
+        calculate_lact = False
+        calculate_clahe = False
+        calculate_gamma = False
+        if image_path in results_dict.keys():
+            if "lact" not in results_dict[image_path]:
+                calculate_lact = True
+            if "clahe" not in results_dict[image_path]:
+                calculate_clahe = True
+            if "gamma" not in results_dict[image_path]:
+                calculate_gamma = True
+        else:
+            results_dict[image_path] = {}
+            calculate_lact = True
+            calculate_clahe = True
+            calculate_gamma = True
+        if calculate_lact or calculate_clahe or calculate_gamma:
+            image = cv.imread(args["dataset_path"]+image_path)
+            label = cv.imread(args["dataset_path"]+label_path)
+            image = cv.resize(image, (resize_shape[0], resize_shape[1]))
+            label = cv.resize(label, (resize_shape[0], resize_shape[1]))
+            merged = np.zeros((resize_shape[1], resize_shape[0]*5, 3), dtype = "uint8")
+            merged[0:resize_shape[1],0*resize_shape[0]:1*resize_shape[0],:] = label
+            merged[0:resize_shape[1],1*resize_shape[0]:2*resize_shape[0],:] = image
+            if calculate_lact:
+                lact_prediction = lact_inference(image)
+                psnr_ = psnr(label, lact_prediction).numpy()
+                ssim_ = ssim(label, lact_prediction).numpy()
+                results_dict[image_path]["lact"] = (psnr_, ssim_)
+                merged[0:resize_shape[1],2*resize_shape[0]:3*resize_shape[0],:] = lact_prediction
+                output_file.write(image_path + "\t" + "lact" + "\t" + str(psnr_) + "\t" + str(ssim_) + "\n")
+            if calculate_clahe:
+                clahe_prediction = clahe_applier(image)
+                psnr_ = psnr(label, clahe_prediction).numpy()
+                ssim_ = ssim(label, clahe_prediction).numpy()
+                results_dict[image_path]["clahe"] = (psnr_, ssim_)
+                merged[0:resize_shape[1],3*resize_shape[0]:4*resize_shape[0],:] = clahe_prediction
+                output_file.write(image_path + "\t" + "clahe" + "\t" + str(psnr_) + "\t" + str(ssim_) + "\n")
+            if calculate_gamma:
+                gamma_prediction = gamma_applier(image)
+                psnr_ = psnr(label, gamma_prediction).numpy()
+                ssim_ = ssim(label, gamma_prediction).numpy()
+                results_dict[image_path]["gamma"] = (psnr_, ssim_)
+                merged[0:resize_shape[1],4*resize_shape[0]:5*resize_shape[0],:] = gamma_prediction
+                output_file.write(image_path + "\t" + "gamma" + "\t" + str(psnr_) + "\t" + str(ssim_) + "\n")
+            cv.putText(merged, "LABEL", (0*resize_shape[0], 40), cv.FONT_HERSHEY_SIMPLEX , 1,  (255,0,0), 2, cv.LINE_AA) 
+            cv.putText(merged, "IMAGE", (1*resize_shape[0], 40), cv.FONT_HERSHEY_SIMPLEX , 1,  (255,0,0), 2, cv.LINE_AA) 
+            cv.putText(merged, "LACT", (2*resize_shape[0], 40), cv.FONT_HERSHEY_SIMPLEX , 1,  (255,0,0), 2, cv.LINE_AA) 
+            cv.putText(merged, "CLAHE", (3*resize_shape[0], 40), cv.FONT_HERSHEY_SIMPLEX , 1,  (255,0,0), 2, cv.LINE_AA) 
+            cv.putText(merged, "GAMMA", (4*resize_shape[0], 40), cv.FONT_HERSHEY_SIMPLEX , 1,  (255,0,0), 2, cv.LINE_AA) 
+            cv.imwrite(args["merged_predictions_path"]+image_path.replace("/", "_").replace(".", "_")+"_merged.jpg", merged)
+    
+    output_file.close()
     
     # Parse result dict and calculate overall metrics
 
-    image = cv.imread("/home/kursad/git/datasets/SICE/Dataset_Part1/1/4.JPG")
-    label = cv.imread("/home/kursad/git/datasets/SICE/Dataset_Part1/Label/1.JPG")
-    image = cv.resize(image, (resize_shape[0], resize_shape[1]))
-    label = cv.resize(label, (resize_shape[0], resize_shape[1]))
+    results_len = len(results_dict)
+    lact_psnr_sum = 0.0
+    lact_ssim_sum = 0.0
+    clahe_psnr_sum = 0.0
+    clahe_ssim_sum = 0.0
+    gamma_psnr_sum = 0.0
+    gamma_ssim_sum = 0.0
+    for key in results_dict.keys():
+        lact_psnr, lact_ssim = results_dict[key]["lact"]
+        clahe_psnr, clahe_ssim = results_dict[key]["clahe"]
+        gamma_psnr, gamma_ssim = results_dict[key]["gamma"]
+        lact_psnr_sum += lact_psnr
+        lact_ssim_sum += lact_ssim
+        clahe_psnr_sum += clahe_psnr
+        clahe_ssim_sum += clahe_ssim
+        gamma_psnr_sum += gamma_psnr
+        gamma_ssim_sum += gamma_ssim
 
-    lact_prediction = lact_inference(image)
-    clahe_output = clahe_applier(image)
-    gamma_output = gamma_applier(image)
+    lact_psnr_avg = lact_psnr_sum / results_len
+    lact_ssim_avg = lact_ssim_sum / results_len
+    clahe_psnr_avg = clahe_psnr_sum / results_len
+    clahe_ssim_avg = clahe_ssim_sum / results_len
+    gamma_psnr_avg = gamma_psnr_sum / results_len
+    gamma_ssim_avg = gamma_ssim_sum / results_len
 
-    print("Lact : ", {"psnr": psnr(label, lact_prediction), "ssim": ssim(label, lact_prediction)})
-    print("Clahe : ", {"psnr": psnr(label, clahe_output), "ssim": ssim(label, clahe_output)})
-    print("Gamma : ", {"psnr": psnr(label, gamma_output), "ssim": ssim(label, gamma_output)})
+    print("Algorithm\t\tPSNR\t\tSSIM")
+    print("LACT\t\t"+str(lact_psnr_avg)+"\t\t"+str(lact_ssim_avg))
+    print("CLAHE\t\t"+str(clahe_psnr_avg)+"\t\t"+str(clahe_ssim_avg))
+    print("GAMMA\t\t"+str(gamma_psnr_avg)+"\t\t"+str(gamma_ssim_avg))
 
 if __name__ == "__main__":
     main()
